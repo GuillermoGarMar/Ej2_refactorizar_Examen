8a60ce1e0795d19e97d8066b89fdddca8fe03edb
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-12-12 09:50:47 +0100

    Crear ambas clases

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -2,25 +2,32 @@ package factorial;
 
 public class Factorial {
 
+    public static void Condicion(int auxn1, int auxn2, int i2) {
+        if (auxn1 == 0) {
+            auxn2 = 1;
+        } else {
+            auxn2 = 1;
+            for (i2 = auxn1; i2 >= 1; i2--) {
+                auxn2 = auxn2 * i2;
+            }
+        }
+        Mostrar(auxn2);
+
+    }
+
+    public static void Mostrar(int x) {
+        System.out.println(x);
+    }
+
     public static void main(String[] args) {
         //hago un format al principio de todo para ordenar el codigo asi entenrderlo mejor
         //Cambio los nombres de las variables usando el rename; j=num1, f=num2 e i se queda igual
-        int num1;
-        int num2;
+        int num1 = 8;
+        int num2 = 0;
 
-        num1 = 8;
-
-        int i;
-        if (num1 == 0) {
-            num2 = 1;
-        } else {
-            num2 = 1;
-            for (i = num1; i >= 1; i--) {
-                num2 = num2 * i;
-            }
-        }
-
-        System.out.println(num2);
+        int i = 0;
+        //creo una clase donde muevo el if
+        Condicion(num1, num2, i);
 
     }
 
